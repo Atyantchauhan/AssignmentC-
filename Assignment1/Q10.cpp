39f01a6a7e0374f2f5632b1e89544bd7af21a76f
Atyant Chuahan <95697409+Atyantchauhan@users.noreply.github.com>
2022-09-02 18:51:30 +0530

    Add files via upload

---
+++ Assignment1/Q10.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int day,month,year;
+	printf("DD/MM/YYYY ");
+	scanf("%d%d%d",&day,&month,&year);
+	printf("Day-%d, Mont-%d, Year-%d",day,month,year);
+		
+	return 0;
+}
---
+++ Assignment1/Q3.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+	printf(" \"mySirG\" ");
+		
+	return 0;
+}
